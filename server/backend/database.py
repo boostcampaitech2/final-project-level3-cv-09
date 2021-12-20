9fea96ee16c4fafa2f197869345df87868eaa2e4
unknown <choihj94@gmail.com>
2021-12-20 18:01:53 +0900

    modify Dockerfile

--- server/backend/database.py
+++ server/backend/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine, insert
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:1234@localhost:3306/food_db?charset=utf8"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:1234@mysql-test:3306/food_db?charset=utf8"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
